5dbf5578447f35367a0a9af39b45a02f372cc28d
Gary <garycouvrand@gmail.com>
2022-07-11 00:30:32 +0200

    feat(auth) reverted change on phone number regex

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -50,10 +50,7 @@ function SignUpBox() {
   const onChangePhoneNumber = (e: any) => {
     setPhoneNumber(e.target.value);
     if (
-      !e.target.value.match(
-        /((?:\+|00)[17](?: |-)?|(?:\+|00)[1-9]\d{0,2}(?: |)?|(?:\+|00)1-\d{3}(?: |-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |-)[0-9]{3}(?: |-)[0-9]{4})|([0-9]{7}))/g
-      )
-    ) {
+      !e.target.value.match(/^\+(?:[0-9]●?){6,14}[0-9]$/g)) {
       setErrorPhoneNumber(true);
     } else {
       setErrorPhoneNumber(false);
